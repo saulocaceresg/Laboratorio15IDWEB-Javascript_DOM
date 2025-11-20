8b753f0ca9b2628713ee24ff4c0091040bae7780
saulocaceresg <scaceresg@unsa.edu.pe>
2025-11-20 14:34:26 -0500

    Commit 2: contenido en index.html y primer ejercicio hecho

---
+++ public-html/scripts/script.js
@@ -0,0 +1,5 @@
+// 3.	Seleccionar el primer elemento <h2> del documento y muestra su texto en la consola
+
+const elemento = document.getElementsByTagName("h2")[0];
+
+console.log("Elemento seleccionado: ", elemento.textContent);
